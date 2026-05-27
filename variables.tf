1468862f97a2563f616cd7b178c234d39a07d031
Jatin Bisht <jatin.bizz1418@gmail.com>
2026-05-27 19:57:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "kubernetes_version" {
   default     = "1.35"
-  description = "kubernetes version"
+  description = "Kubernetes version for the EKS cluster"
 }
 
 variable "vpc_cidr" {
   default     = "10.0.0.0/16"
-  description = "default CIDR range of the VPC"
+  description = "Default CIDR range of the VPC"
 }
 
 variable "aws_region" {
-  default = "ap-south-1"
-  description = "aws region"
+  default     = "ap-south-1"
+  description = "AWS region to deploy resources"
 }
